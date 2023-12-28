aa0e23b07064d269d43dcf78e68c598bc021d5a7
MERAJ HASSAN <101815583+merajafnan@users.noreply.github.com>
2023-12-28 17:11:05 +0530

    next popular cateogary

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,7 +49,7 @@ const Navbar = () => {
     </>
     return (
         <header className="max-w-full container mx-auto fixed top-0 left-0 right-0 transition-all duration-200 ease-in-out">
-            <div className="navbar xl:px-24">
+            <div className={'navbar xl:px-24 ${isSticky ? "shadow-md bg-base-100 transition-all duration-300 ease-in-out" : ""}'}>
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
